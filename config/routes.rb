c8c9d5694625b11fee709dec9ca89095fc258957
Sean Gair <gair.sean@gmail.com>
2014-11-14 08:15:13 -0600

    Part 2 completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   resources :products
 
+  get '/', to: 'finder#index'
+  get '/alphabetized', to: 'finder#alphabetized'
+  get '/missing_email', to: 'finder#missing_email'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
