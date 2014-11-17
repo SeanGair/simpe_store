901c603ff50d05f91579c5fd7391e23e25d9fb34
Sean Gair <gair.sean@gmail.com>
2014-11-17 16:58:36 -0600

    Part 3 Completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111170059) do
+ActiveRecord::Schema.define(version: 20141117221927) do
 
   create_table "customers", force: true do |t|
     t.string   "full_name"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20141111170059) do
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "province_id"
   end
+
+  add_index "customers", ["province_id"], name: "index_customers_on_province_id"
 
   create_table "products", force: true do |t|
     t.string   "title"
@@ -29,6 +32,12 @@ ActiveRecord::Schema.define(version: 20141111170059) do
     t.string   "text"
     t.decimal  "price"
     t.integer  "stock_qunatity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "provinces", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
